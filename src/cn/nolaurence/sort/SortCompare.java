9d6a6903938129cdcb73da640b994ffe2f445348
nolaurence <nolaurence@outlook.com>
2021-04-12 20:27:40 +0800

    add

--- src/cn/nolaurence/sort/SortCompare.java
+++ src/cn/nolaurence/sort/SortCompare.java
@@ -4,5 +4,33 @@ package cn.nolaurence.sort;
 import edu.princeton.cs.algs4.*;
 
 public class SortCompare {
-    public static double
+    public static double time(String alg, Double[] a) {
+        Stopwatch timer = new Stopwatch();
+        if (alg.equals("Insertion"))  Insertion.sort(a);
+        if (alg.equals("Selection"))  Selection.sort(a);
+        return timer.elapsedTime();
+    }
+
+    public static double timeRandomInput(String alg, int N, int T) {
+        double total = 0.0;
+        Double[] a = new Double[N];
+        for (int t = 0; t < T; t++) {
+            for (int i = 0; i < N; i++) {
+                a[i] = StdRandom.uniform();
+            }
+            total += time(alg, a);
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        String alg1 = "Insertion";
+        String alg2 = "Selection";
+        int N = Integer.parseInt("1000");
+        int T = Integer.parseInt("100");
+        double t1 = timeRandomInput(alg1, N, T);
+        double t2 = timeRandomInput(alg2, N, T);
+        StdOut.printf("For %d random Doubles\n    %s is",  N, alg1);
+        StdOut.printf(" %.1f times faster than %s\n", t2/t1, alg2);
+    }
 }
